e43608b99dc9ccdf7aec64bf72a2caf2820a9838
Steve Mickeler <smickeler@globeandmail.com>
2020-01-30 10:10:58 -0500

    force bucket name to be lowercase

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ locals {
 
 resource "aws_s3_bucket" "artifact" {
   # S3 bucket cannot be longer than 63 characters
-  bucket = substr("codepipeline-${local.aws_region}-${local.account_id}-${var.name}", 0, 63)
+  bucket = lower(substr("codepipeline-${local.aws_region}-${local.account_id}-${var.name}", 0, 63))
   acl    = "private"
 
   lifecycle_rule {
